1ca29a0c596cf07ab8c3c8ae5ab8c30308915ea9
bmalkes <bmalkes@gmail.com>
2025-03-25 09:42:18 +0200

    feat:add readme

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-  audience: "http://localhost:8000",
+  audience: "https://casacentral-backend.onrender.com",
   issuerBaseURL: "https://dev-a0yd7dci5dy456df.us.auth0.com",
   tokenSigningAlg: "RS256",
 });
